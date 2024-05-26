9a08498f42de929996721683803110c3f62ee52f
Henry Ripple <hripple46@gmail.com>
2024-05-26 15:13:48 -0500

    stylistic changes to header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,18 +1,18 @@
 export default function Header() {
   return (
     <>
-      <ul className="w-full h-12 bg-red-300 flex justify-center justify-evenly align-center top-0 fixed">
-        <li className="bg-red-300 text-white text-3xl font-bold underline">
-          Home
-        </li>
-        <li className="bg-red-300 text-white text-3xl font-bold underline">
-          Collection
-        </li>
-        <li className="bg-red-300 text-white text-3xl font-bold underline">
-          Other
-        </li>
+      <ul className="w-full h-24 bg-blue-300 flex justify-center lg:justify-start items-center top-0 fixed">
+        <div className=" h-full hover:bg-blue-400 flex items-center pl-2">
+          <li className=" text-white text-3xl pr-2 ">Stephen Ripple</li>
+        </div>
+        <div className=" h-full hover:bg-blue-400 flex items-center pl-2">
+          <li className="text-white text-2xl pr-2">Collection</li>
+        </div>
+        <div className=" h-full hover:bg-blue-400 flex items-center pl-2">
+          <li className=" text-white text-2xl pr-2">Other</li>
+        </div>
       </ul>
-      <div className="h-12"></div>
+      <div className="h-24"></div>
     </>
   );
 }
