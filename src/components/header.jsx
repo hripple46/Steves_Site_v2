84ec317cb23e4074aa8085eff3228c6e5df7c1ec
Henry Ripple <hripple46@gmail.com>
2024-06-05 12:47:51 -0500

    added dropdown to 'collection'

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
+
 export default function Header() {
+  const [isCollectionHovered, setIsCollectionHovered] = useState(false);
+
+  const renderCollection = () => {
+    if (isCollectionHovered) {
+      return (
+        <ul className="absolute top-full bg-white w-full left-0">
+          <li className="hover:bg-gray-200">Down and Away</li>
+          <li className="hover:bg-gray-200">The Crying Wolf</li>
+        </ul>
+      );
+    }
+  };
+
   return (
     <>
       <ul className="w-full h-24 bg-blue-300 flex justify-center lg:justify-start items-center top-0 fixed">
         <div className=" h-full hover:bg-blue-400 flex items-center pl-2">
           <li className=" text-white text-3xl pr-2 ">Stephen Ripple</li>
         </div>
-        <div className=" h-full hover:bg-blue-400 flex items-center pl-2">
-          <li className="text-white text-2xl pr-2">Collection</li>
+        <div
+          onMouseEnter={() => setIsCollectionHovered(true)}
+          onMouseLeave={() => setIsCollectionHovered(false)}
+          className="relative h-full hover:bg-blue-400 flex items-center pl-2 pr-2"
+        >
+          <li className="text-white text-2xl ">Collection</li>
+          {renderCollection()}
         </div>
         <div className=" h-full hover:bg-blue-400 flex items-center pl-2">
           <li className=" text-white text-2xl pr-2">Other</li>
