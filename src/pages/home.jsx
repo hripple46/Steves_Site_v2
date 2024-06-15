f3d5bf84afbeb1ebb7ca4b3e5396397cbec042c7
Henry Ripple <hripple46@gmail.com>
2024-06-14 22:16:17 -0500

    added profile image and structure of home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,16 @@ function HomePage() {
   return (
     <div>
       <Header />
-      <img src={ProfilePic} alt="Stephen's Profile Picture" />
+      <div className="w-screen h-screen flex justify-center items-center">
+        <img
+          className="h-auto w-1/3 rounded-md"
+          src={ProfilePic}
+          alt="Stephen's Profile Picture"
+        />
+        <div className="h-1/3 w-1/3 rounded-md bg-slate-600/25 flex items-center justify-center">
+          <p>Here's where the author description goes.</p>
+        </div>
+      </div>
     </div>
   );
 }
