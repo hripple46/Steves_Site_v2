73584f49101182e9aaecedaed39ac2f071ee8cac
Henry Ripple <hripple46@gmail.com>
2024-06-24 18:43:55 -0500

    changed text color

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -25,12 +25,10 @@ export default function Header() {
   return (
     //header has height of 24 (tailwindcss); add margins to top of other components to push down same height
     <>
-      <ul className="z-10 w-full h-24 bg-blue-300/75 flex backdrop-blur-md justify-center lg:justify-start items-center top-0 fixed">
+      <ul className="z-10 w-full text-black h-24 bg-blue-300/75 flex backdrop-blur-md justify-center lg:justify-start items-center top-0 fixed">
         <Link to="/" className="h-full">
           <div className=" h-full hover:bg-blue-400 flex items-center pl-2">
-            <li className="pl-8 text-white text-3xl pr-8 text-center ">
-              Stephen Ripple
-            </li>
+            <li className="pl-8  text-3xl pr-8 text-center ">Stephen Ripple</li>
           </div>
         </Link>
 
@@ -39,11 +37,11 @@ export default function Header() {
           onMouseLeave={() => setIsCollectionHovered(false)}
           className="relative h-full hover:bg-blue-400 hover:cursor-pointer  flex items-center pl-2 pr-2"
         >
-          <li className="text-white text-2xl ">Collection</li>
+          <li className="text-black text-2xl ">Collection</li>
           {renderCollection()}
         </div>
         <div className=" h-full hover:bg-blue-400 hover:cursor-pointer flex items-center pl-2">
-          <li className=" text-white text-2xl pr-2">Other</li>
+          <li className=" text-black text-2xl pr-2">Other</li>
         </div>
       </ul>
     </>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,7 +15,7 @@ function HomePage() {
       <div className="overflow-hidden box-border">
         <div className="absolute bg-boat bg-cover w-full border-box">
           <div className="backdrop-blur-sm w-full h-screen flex flex-col justify-start items-center overflow-hidden  mt-24">
-            <h1 className="lg:text-6xl text-4xl pt-24 pb-12 overflow-hidden">
+            <h1 className="lg:text-6xl text-4xl pt-24 text-white pb-12 overflow-hidden">
               STEPHEN TIMOTHY RIPPLE
             </h1>
             <div className="flex justify-evenly overflow-hidden">
